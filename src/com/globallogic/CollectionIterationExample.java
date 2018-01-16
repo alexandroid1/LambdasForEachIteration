d7fdc65e694ba28b22367bc24f9af9ed78c83669
alexandroid1 <alex123456>
2018-01-16 14:27:44 +0200

    numbers

--- src/com/globallogic/CollectionIterationExample.java
+++ src/com/globallogic/CollectionIterationExample.java
@@ -2,6 +2,7 @@ package com.globallogic;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class CollectionIterationExample {
 
@@ -14,20 +15,48 @@ public class CollectionIterationExample {
                 new Person("Matthew", "Arnold", 39)
         );
 
-        System.out.println("Using for loop");
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // external iterators
+
+        System.out.println("Using for loop (external iterator)");
+        // people
         for (int i = 0; i < people.size(); i++) {
             System.out.println(people.get(i));
         }
+        // numbers
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
 
+
+        // external iterators also
+        // people
         System.out.println("");
         System.out.println("Using foreach loop (external iterator)");
         for (Person p : people) {
             System.out.println(p);
         }
+        // numbers
+        for(int e:numbers){
+            System.out.println(e);
+        }
 
+        // people
         System.out.println("");
         System.out.println("Using lambdas for each (external iterator)");
         people.forEach(p -> System.out.println(p));
+
+        //internal iterator
+        // numbers
+        numbers.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer value) {
+                System.out.println(value);
+            }
+        });
+        // numbers
+        numbers.forEach(n -> System.out.println(n));
 
         System.out.println("");
         System.out.println("Using lambdas for each (external iterator) + parallel");
@@ -35,9 +64,11 @@ public class CollectionIterationExample {
 
         System.out.println("");
         System.out.println("Using for each method references (internal iterator) - can be executed in parallel");
+        // people
         people.forEach(System.out::println);
+        // numbers
+        numbers.forEach(System.out::println);
 
     }
-
 
 }
